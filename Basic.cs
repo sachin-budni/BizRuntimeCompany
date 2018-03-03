7f0e47c85bd780579c7ece1b98c922ba28ed6060
sachin budni <sachinswapna143@gmail.com>
2018-03-03 14:05:19 +0530

    Basic ang loop file

---
+++ Basic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_1
+{
+    class Basic
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            byte k = 2;
+            short l = 30;
+            long n = 109990L;
+            float m = 20.3F;
+            double j = 10.4;
+            unsafe
+            {
+                int* Jk = &i;
+
+                Console.WriteLine(i + k);
+                Console.WriteLine(i - k);
+                Console.WriteLine(i * k);
+                Console.WriteLine(i / k);
+                Console.WriteLine();
+
+                Console.WriteLine(j);
+
+                Console.WriteLine(i);
+                i++;
+                Console.WriteLine(i);
+                Console.WriteLine(++i);
+                i--;
+                Console.WriteLine(i);
+                Console.WriteLine(--i);
+
+                Console.WriteLine(k);
+                Console.WriteLine(n);
+                Console.WriteLine(l);
+                Console.WriteLine(m);
+                Console.WriteLine();
+                Console.WriteLine(*Jk);
+                Console.WriteLine((int)Jk);
+
+                Console.WriteLine(i > k ? true : false);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Loops.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Loops
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any Number");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (i < 0)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Enter more than 0");
+                }
+                else
+                {
+                    Console.WriteLine("Enter valid number");
+                }
+            }
+            else if (i < 35)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (i >= 35)
+            {
+                if (i >= 35 && i < 45)
+                {
+                    Console.WriteLine("Just Pass");
+                }
+                else if (i >= 45 && i < 60)
+                {
+                    Console.WriteLine("Second Class");
+                }
+                else if (i >= 60 && i < 75)
+                {
+                    Console.WriteLine("First Class");
+                }
+                else if (i >= 75 && i <= 99)
+                {
+                    Console.WriteLine("Distinction");
+                }
+                else if (i == 100)
+                {
+                    Console.WriteLine("Out of Out");
+                }
+            }
+            switch (i)
+            {
+                case 10:
+                    Console.WriteLine("it is number 10 ");
+                    break;
+                case 20:
+                    Console.WriteLine("it is number 20 ");
+                    break;
+                case 30:
+                    Console.WriteLine("it is number 30");
+                    break;
+                default:
+                    Console.WriteLine("it is not 10,20,30 number");
+                    break;
+            }
+            Console.WriteLine("for Loop");
+            for(int k=1; k<=3; k++)
+            {
+                for(int m=1; m<=3; m++)
+                {
+                    Console.WriteLine(k + " : " + m);
+                }
+            }
+            int n = 1;
+            Console.WriteLine("while Loops");
+            while(n<=3)
+            {
+                int o = 1;
+                while(o<=3)
+                {
+                    Console.WriteLine(n+" : "+o);
+                    o++;
+                }
+                n++;
+            }
+            Console.WriteLine("Do While Loop");
+            int x = 1;
+            do
+            {
+                int y = 1;
+                do
+                {
+                    Console.WriteLine(x+":"+y);
+                    y++;
+                }
+                while (y < 3);
+                x++;
+            }
+            while (x < 3);
+            Console.WriteLine("break Statement");
+            for(int p=1;p<=5; p++)
+            {
+                if(p==4)
+                {
+                    break;
+                }
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("Continues Statement");
+            for(int p=1; p<=5; p++)
+            {
+                if(p==3)
+                {
+                    continue;
+                }
+                Console.WriteLine(p);
+            }
+        good:
+            Console.WriteLine("enter proper age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age<1)
+            {
+                goto good;
+            }
+            else
+            {
+                Console.WriteLine("your age is = " + age);
+            }
+            Console.ReadLine();
+        }
+    }
+}
