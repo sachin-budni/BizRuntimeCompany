ebff00c3e6503347d42d181c7bd0621492d28267
sachin budni <sachinswapna143@gmail.com>
2018-03-21 16:10:22 +0530

    using UDP client

---
+++ NetWork Programing/UDPCilent/UDPCilent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPCilent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                IPAddress broadcast = IPAddress.Parse("192.168.1.255");
+                string s1 = Console.ReadLine();
+                char[] chars = s1.ToCharArray();
+                byte[] sendbuf = Encoding.ASCII.GetBytes(chars);
+                IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+                s.SendTo(sendbuf, ep);
+
+                Console.WriteLine("Message sent to the broadcast address");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NetWork Programing/UDPServer/UDPServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+    class Program
+    {
+        private const int listenPort = 11000;
+
+        private static void StartListener()
+        {
+            bool done = false;
+
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (!done)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    Console.WriteLine("Received broadcast from {0} :\n {1}\n",groupEP.ToString(),Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+        public static int Main()
+        {
+            StartListener();
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
+
