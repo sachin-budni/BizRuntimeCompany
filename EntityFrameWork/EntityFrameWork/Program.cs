8fa8a10347bd3cb83d426810d84f148f05633d4c
sachin budni <sachinswapna143@gmail.com>
2018-03-29 20:09:24 +0530

    adding Entity frameWork File

---
+++ EntityFrameWork/EntityFrameWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Simple s1=new Simple())
+            {
+
+            }
+        }
+    }
+}
