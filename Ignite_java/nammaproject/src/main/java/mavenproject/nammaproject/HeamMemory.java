7106b5f3a44addb046d416e379543f60544e6e6a
sachin budni <sachinswapna143@gmail.com>
2018-04-18 20:25:49 +0530

    adding ignite stateful

--- Ignite_java/nammaproject/src/main/java/mavenproject/nammaproject/HeamMemory.java
+++ Ignite_java/nammaproject/src/main/java/mavenproject/nammaproject/HeamMemory.java
@@ -5,7 +5,7 @@ public class HeamMemory
 	public static void main(String[] args) 
 	{
 		long heapSize = Runtime.getRuntime().totalMemory();
-
+//		long heapSize=Runtime.getRuntime().maxMemory();
         System.out.println("Heap Size = " + heapSize/(1024*1024));
 	}
 }
